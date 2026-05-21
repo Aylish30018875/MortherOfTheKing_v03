76e1adedb3ee84a40cb5cdcd8a803f377db1e9e3
Jasper Knott <30095212@tafe.wa.edu.au>
2026-05-21 14:59:20 +0800

    Feat: Added enemy script
    Added the script for enemy pathfinding

---
+++ Assets/Scripts/Jasper/Enemy.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 2;
+    public int health = 1;
+    public Transform[] wayPoints;
+
+    public int currentWayPoint = 0;
+
+    private void Update()
+    {
+        if (wayPoints == null || wayPoints.Length == 0)
+        {
+            return;
+        }
+        Transform target = wayPoints[currentWayPoint];
+        Vector3 dir = (target.position - transform.position).normalized;
+
+        transform.position += dir * speed * Time.deltaTime;
+
+        if (Vector3.Distance(transform.position, target.position) < 0.05f)
+        {
+            currentWayPoint++;
+            if (currentWayPoint >= wayPoints.Length)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
